05093e57302ad2f10819929e1d8325f7839cd147
Lu Wang <lw7@rice.edu>
2009-10-26 15:51:43 -0500

    msg here

---
+++ git-project.r
@@ -0,0 +1,17 @@
+# git project
+
+names <- read.csv("baby-names.csv", header = T, stringsAsFactors = F)
+
+# extracting all data related to my name
+my_name <- subset(names, name == "Lu" & sex == "girl")
+
+# plotting my name's popularity over time
+library(ggplot2)
+qplot(year, prop, data = my_name, geom = "line", main = "Percentage of children named Lu, by year")
+
+
+# for later
+both_names <- rbind(my_name, second_name)
+
+qplot(year, prop, data = both_names, geom = "line", colour = name, main = "Popularity of group member names over time")
+ggsave("names.pdf", width = 5, height = 4)
